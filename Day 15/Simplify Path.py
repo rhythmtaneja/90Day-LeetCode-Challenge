1a8d8d30f39c242a8fde0982c9acce621ede92e9
rhythmtaneja <168122925+rhythmtaneja@users.noreply.github.com>
2025-06-10 19:29:09 +0530

    Update Simplify Path.py

--- Day 15/Simplify Path.py
+++ Day 15/Simplify Path.py
@@ -1,1 +1,29 @@
+class Solution {
+    public String simplifyPath(String path) {
+       Stack<String> st = new Stack<>();
+       String[] parts = path.split("/");
 
+       for (String part: parts){
+        if (part.equals("") || part.equals(".")){
+            continue;
+        } else if (part.equals("..")){
+            if (!st.isEmpty()){
+            st.pop();
+            }
+        } else {
+            st.push(part);
+        }
+       }
+       StringBuilder result = new StringBuilder();
+       for (String dir : st){
+        result.append("/").append(dir);
+       }
+       
+       if (result.length() == 0) {
+    return "/";
+} else {
+    return result.toString();
+}
+
+    }
+}

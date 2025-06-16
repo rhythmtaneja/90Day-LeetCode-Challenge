8b3451acb3ab88d36a3b0393c30a81467fb21631
rhythmtaneja <168122925+rhythmtaneja@users.noreply.github.com>
2025-06-17 00:08:11 +0530

    Update Linked List Cycle.py

--- Day 22/Linked List Cycle.py
+++ Day 22/Linked List Cycle.py
@@ -11,13 +11,16 @@
  */
 public class Solution {
     public boolean hasCycle(ListNode head) {
-        ListNode temp = head;
-        if (head == null || head.next == null){
-            return false;
-        }
-        temp = temp.next;
-        if (temp != null) {
-            return true;
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while(fast != null && fast.next != null ){
+            fast = fast.next.next;
+            slow = slow.next;
+      
+            if (fast == slow){
+                return true;
+            }
         }
         return false;
     }

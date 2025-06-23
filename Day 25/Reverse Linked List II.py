3bc882cd03b603c8be0f1d81af06b99207cbe046
rhythmtaneja <168122925+rhythmtaneja@users.noreply.github.com>
2025-06-23 23:37:25 +0530

    Create Reverse Linked List II.py

---
+++ Day 25/Reverse Linked List II.py
@@ -0,0 +1,32 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode reverseBetween(ListNode head, int left, int right) {
+        if (head == null || left == right){
+            return head;
+        }
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode prev = dummy;
+        for (int i = 0; i < left - 1; i++){
+            prev = prev.next; //stops at 1 in array 
+        }
+        ListNode cur = prev.next; // cur = 2
+
+        for (int i = 0; i < right - left; i++) {
+            ListNode temp = cur.next;
+            cur.next = temp.next;
+            temp.next = prev.next;
+            prev.next = temp;
+        }
+        return dummy.next;
+    }
+}
